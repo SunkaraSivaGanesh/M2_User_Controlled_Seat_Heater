e8a8f735a35aa7540d0912c8d3624f3308a50d26
SunkaraSivaGanesh <101381519+SunkaraSivaGanesh@users.noreply.github.com>
2022-04-21 11:40:54 +0530

    Add files via upload
    added

---
+++ 3_Implementation/fuse.c
@@ -0,0 +1,14 @@
+/*
+ * fuse.c
+ *
+ * Created: 21-04-2022 10.18.28 AM
+ *  Author: Siva
+ */ 
+#include <avr/io.h>
+
+FUSES = {
+	.low = LFUSE_DEFAULT,
+	.high = HFUSE_DEFAULT,
+	.extended = EFUSE_DEFAULT
+};
+
